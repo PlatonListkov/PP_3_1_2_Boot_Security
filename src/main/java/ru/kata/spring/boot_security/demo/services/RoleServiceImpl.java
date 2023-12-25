8ed91556183c9fc2d0f659a6a7637d3ffc45121b
Platon Listkov <platon.listkov@gmail.com>
2023-12-25 13:24:15 +0300

    class RoleServiceImpl
    - удали @Transactional над классом - вешай точечно над методами которые модифицируют БД - OK!

--- src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.kata.spring.boot_security.demo.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.RoleDao;
@@ -9,7 +8,6 @@ import ru.kata.spring.boot_security.demo.models.Role;
 import java.util.Set;
 
 @Service
-@Transactional
 public class RoleServiceImpl implements RoleService {
 
     private final RoleDao roleDao;
